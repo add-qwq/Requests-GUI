af4008efd60e07f091e4ffd6022bb2378f72ba4c
add-qwq <3106316636@qq.com>
2025-05-17 21:03:00 +0800

    Delete Requests-GUI-CN directory

--- Requests-GUI-CN/gui/utils.py
+++
@@ -1,8 +0,0 @@
-# 工具函数（如 JSON 格式化）
-def format_json(text):
-    try:
-        import json
-        obj = json.loads(text)
-        return json.dumps(obj, indent=2, ensure_ascii=False)
-    except:
-        return text
